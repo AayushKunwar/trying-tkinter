a5b234564c55fdf69ea591b3c5c6bd09c994cdc7
Aayush Kunwar <kunwar_aayush@live.com>
2023-05-29 10:48:11 +0545

    changing the code org1st stable

--- drawing/main.py
+++ drawing/main.py
@@ -11,6 +11,9 @@ class Button:
     def draw_button(self):
         self.canvas.create_rectangle(self.x, self.y, 30, 30, fill="blue")
 
+    def update_coords(self, posx, posy):
+        pass
+
 
 class Component:
     def __init__(self, canvas, x, y):
@@ -19,16 +22,32 @@ class Component:
         self.y = y
         self.width = 50
         self.height = 50
+        self.item = None
         self.tag = "component"
+        # self.draw_card()
+        self.buttons = []
         self.draw_card()
 
+    # this for initialization only
     def draw_card(self, color="red"):
         reference = self.canvas.create_rectangle(
             self.x, self.y, self.width + self.x, self.height + self.y, fill=color
         )
         foo = Button(self.canvas, 10, 10)
         # self.add_button(foo)
-        return reference
+        self.item = reference
+
+    def update_coords(self, x, y):
+        width = self.width
+        height = self.height
+        # position = coords[0], coords[1]
+
+        x1 = x - width / 2
+        y1 = y - height / 2
+        x2 = x + width / 2
+        y2 = y + height / 2
+
+        self.canvas.coords(self.item, x1, y1, x2, y2)
 
     def on_click(self, event):
         print("clicked this ")
@@ -47,17 +66,29 @@ class Main_Can(tk.Canvas):
         card2 = Component(canvas=self, x=100, y=200)
         # button = tk.Button(card1, text="what what")
         # button.place(x=2, y=2)
+        self.components = {}
+        self.components.update({card1.item: card1})
+        self.components.update({card2.item: card2})
 
-        self.buttons = []
+        # this to be called only once?
+        # self.draw_components()
+
         self.bind("<ButtonPress-1>", self.on_drag_start)
         self.bind("<B1-Motion>", self.on_drag)
         self.bind("<ButtonRelease-1>", self.on_drag_stop)
+
+    def draw_components(self):
+        # print(self.components)
+        for component in self.components.values():
+            component.draw_card()
 
     def on_drag_stop(self, event):
         self.active = None
 
     def on_drag_start(self, event):
+        # can be component OR BUTTONS
         item = self.find_closest(event.x, event.y)
+        print(item)
         try:
             # item = self.find_withtag("current")
             x1, y1, x2, y2 = self.coords(item[0])
@@ -80,7 +111,12 @@ class Main_Can(tk.Canvas):
         y2 = event.y + height / 2
         # x=self.winfo_x() + (event.x - self.drag_data["x"]),
 
-        self.coords(self.active, x1, y1, x2, y2)
+        try:
+            self.components[self.active].update_coords(event.x, event.y)
+        except KeyError:
+            # print(self.active)
+            pass
+        # self.coords(self.active, x1, y1, x2, y2)
 
 
 root = tk.Tk()
